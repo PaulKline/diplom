6e86c908e6da79b8b433bff768706c865377621a
PaulKline <trb34074@yandex.ru>
2019-08-23 00:31:37 +0500

    готовая первая страница + адаптивы

--- src/js/main.js
+++ src/js/main.js
@@ -9,57 +9,6 @@ $(document).ready(function () {
 			$('#nav_list').children('a').removeClass('#submenu a');
 		});
 	});
-});
-
-$('.owl-carousel').owlCarousel({
-	loop: true,
-	margin: 10,
-	items: 1,
-	onInitialized: updateTitles,
-	onTranslated: updateTitles
-});
-jQuery(document).ready(function () {
-
-
-	// 1) ASSIGN EACH 'DOT' A NUMBER
-	dotcount = 1;
-
-	jQuery('.owl-dot').each(function () {
-		jQuery(this).addClass('dotnumber' + dotcount);
-		jQuery(this).attr('data-info', dotcount);
-		dotcount = dotcount + 1;
-	});
-
-	// 2) ASSIGN EACH 'SLIDE' A NUMBER
-	slidecount = 1;
-
-	jQuery('.owl-item').not('.cloned').each(function () {
-		jQuery(this).addClass('slidenumber' + slidecount);
-		slidecount = slidecount + 1;
-	});
-
-	// SYNC THE SLIDE NUMBER IMG TO ITS DOT COUNTERPART (E.G SLIDE 1 IMG TO DOT 1 BACKGROUND-IMAGE)
-	jQuery('.owl-dot').each(function () {
-
-		grab = jQuery(this).data('info');
-
-		slidegrab = jQuery('.slidenumber' + grab + ' img').attr('src');
-		console.log(slidegrab);
-
-		jQuery(this).css("background-image", "url(" + slidegrab + ")");
-
-	});
-
-	// THIS FINAL BIT CAN BE REMOVED AND OVERRIDEN WITH YOUR OWN CSS OR FUNCTION, I JUST HAVE IT
-	// TO MAKE IT ALL NEAT 
-	amount = jQuery('.owl-dot').length;
-	gotowidth = 30 / amount;
-
-	jQuery('.owl-dot').css("width", gotowidth + "%");
-	newwidth = jQuery('.owl-dot').width();
-	jQuery('.owl-dot').css("height", newwidth + "px");
-
-
 	$('.sails-slider').slick({
 		slideToShow: 1,
 		arrows: false,
@@ -68,10 +17,85 @@ jQuery(document).ready(function () {
 		dotsClass: 'top-dots',
 		appendDots: $('.top-dots')
 	});
+	$('.feedback-slider').slick({
+		arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		draggable: false,
+		centerMode: true,
+		variableWidth: true,
+		appendDots: $('.feedback-dots'),
+		dotsClass: 'slick-dots slider__dots',
+		customPaging: function (slick, index) {
+			var image = $(slick.$slides[index]).find('.slider__img').attr('src');
+			return '<img src="' + image + '" alt="" /> '
+		}
+	});
+	$('.header__slider').slick({
+		arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		dots: true,
+		dotsClass: 'slick-dots slider__dots',
+		customPaging: function (slick, index) {
+			var image = $(slick.$slides[index]).find('.slider__img').attr('src');
+			return '<img src="' + image + '" alt="" />'
+		}
+	});
+	$(".tab_item").not(":first").hide();
+	$(".wrapper .tab").click(function () {
+		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+
+	$('.tab1').click(function () {
+		$('.tabs-block1').css("display", "inline-block"),
+			$('.tabs-block2').hide(),
+			$('.tabs-block3').hide(),
+			$('.tabs-block4').hide()
+	});
+	$('.tab2').click(function () {
+		$('.tabs-block2').css("display", "inline-block"),
+			$('.tabs-block1').hide(),
+			$('.tabs-block3').hide(),
+			$('.tabs-block4').hide()
+	});
+	$('.tab3').click(function () {
+		$('.tabs-block3').css("display", "inline-block"),
+			$('.tabs-block1').hide(),
+			$('.tabs-block2').hide(),
+			$('.tabs-block4').hide()
+	});
+	$('.tab4').click(function () {
+		$('.tabs-block4').css("display", "inline-block"),
+			$('.tabs-block1').hide(),
+			$('.tabs-block2').hide(),
+			$('.tabs-block3').hide()
+	});
+	$(".menu__showsub").click(function () {
+		$(".menu__showsub2").toggle();
+		$(".menu__showsub3").toggle();
+		$(".menu__showsub4").toggle();
+		$(".menu__showsub5").toggle();
+		$(".menu__showsub6").toggle();
+	});
+	function updateTitles() {
+		const $curr = $('.owl-carousel').find('.owl-item.active');
+		$('.prev').text($curr.prev('.owl-item').text());
+		$('.next').text($curr.next('.owl-item').text());
+	}
+});
+$('.button').mouseover(function(){
+	$(this).css("filter", 'opacity(90%');
+});
+$('.button').mouseout(function () {
+	$(this).css("filter", 'opacity(100%');
 });
 
-function updateTitles() {
-	const $curr = $('.owl-carousel').find('.owl-item.active');
-	$('.prev').text($curr.prev('.owl-item').text());
-	$('.next').text($curr.next('.owl-item').text());
-}
+$('.burger-btn').on('click', function(e){
+	e.preventDefault;
+	$(this).toggleClass('.burger-btn-active');
+});

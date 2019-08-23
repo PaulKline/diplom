040bddd0d4de35196b450ef478306f5d00362349
PaulKline <trb34074@yandex.ru>
2019-08-23 15:10:43 +0500

    сделал модальные окна и футер

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,125 @@
 $(document).ready(function () {
+	var callorder = document.querySelector('#callorder-anchor');
+	var modalOne = document.querySelector('#modal-one');
+	var closeOne = document.querySelector('#close__one');
+	var invite = document.querySelector('#invite-anchor');
+	var modalTwo = document.querySelector('#modal-two');
+	var closeTwo = document.querySelector('#close__two');
+	/* валидация формы секции brif */
+	$('.brif__form').validate({
+		errorElement: "div",
+		errorClass: "invalid",
+		rules: {
+			username: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			username: {
+				required: 'обязательно укажите ваше имя',
+				minlength: jQuery.validator.format('Осталось: {0}'),
+				maxlength: 'максимальное число символов - 16'
+			},
+			phone: 'укажите номер вашего телефона'
+		},
+		submitHandler: function (form) {
+			$.ajax({
+				url: 'mail.php',
+				type: 'POST',
+				data: $('.brif__form').serialize(),
+				success: function (data) {
+					$('.brif__form')[0].reset();
+				}
+			});
+		}
+	});
+	/* валидация формы модального окна "заказать звонок"  */
+	$('.modal-callorder-form').validate({
+		errorElement: "div",
+		errorClass: "invalid",
+		rules: {
+			username: {
+				required: true,
+				minlength: 2,
+				maxlength: 16
+			},
+			phone: {
+				required: true
+			}
+		},
+		messages: {
+			username: {
+				required: 'обязательно укажите ваше имя',
+				minlength: jQuery.validator.format('Осталось: {0}'),
+				maxlength: 'максимальное число символов - 16'
+			},
+			phone: 'укажите номер вашего телефона'
+		},
+		submitHandler: function (form) {
+			$.ajax({
+				url: 'mail.php',
+				type: 'POST',
+				data: $('.modal-callorder-form').serialize(),
+				success: function (data) {
+					$('.modal-callorder-form')[0].reset();
+				}
+			});
+		}
+	});
+	/* настрока формы модального окна "вызывать замерщика" */
+		$('.modal-invite-form').validate({
+			errorElement: "div",
+			errorClass: "invalid",
+			rules: {
+				username: {
+					required: true,
+					minlength: 2,
+					maxlength: 16
+				},
+				phone: {
+					required: true
+				}
+			},
+			messages: {
+				username: {
+					required: 'обязательно укажите ваше имя',
+					minlength: jQuery.validator.format('Осталось: {0}'),
+					maxlength: 'максимальное число символов - 16'
+				},
+				phone: 'укажите номер вашего телефона'
+			},
+			submitHandler: function (form) {
+				$.ajax({
+					url: 'mail.php',
+					type: 'POST',
+					data: $('.modal-invite-form').serialize(),
+					success: function (data) {
+						$('.modal-invite-form')[0].reset();
+					}
+				});
+			}
+		});
+	/* подключение масок */
+	$('.phones').mask('+7(999)999-99-99');
+	/* вызов модальных окон */
+	callorder.addEventListener('click', function (){
+		modalOne.classList.add('modal-one__active');
+	});
+	closeOne.addEventListener('click', function () {
+		modalOne.classList.remove('modal-one__active');
+	});
+	invite.addEventListener('click', function () {
+		modalTwo.classList.add('modal-two__active');
+	});
+	closeTwo.addEventListener('click', function () {
+		modalTwo.classList.remove('modal-two__active');
+	});
+	/* всплывающее меню */
 	$(function () {
 		$('#nav_list').hover(function () {
 			$('#nav_list').children('ul').stop(false, false).fadeIn(200);
